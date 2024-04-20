64a5afcebecca46b9c1362420086ec719e92782b
AryanAAB <aryanbansal310@gmail.com>
2024-04-20 12:07:37 +0530

    Added a Math Library

---
+++ Aptitude Test/basic_math_operations/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='basic_math_operations',
+    version='1.0.0',
+    packages=find_packages(),
+    description='A Python package for basic mathematical operations',
+    url='https://github.com/AryanAAB/basic_math_operations',
+    author='Aryan Bansal',
+    author_email='your_email@example.com',
+    license='MIT',
+)
